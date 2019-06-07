0eb31053aa05a197ea754dff02adbb7a7940d30a
Kelvin Wong <manchunw@gmail.com>
2019-06-07 22:47:05 +0800

    Add Order Item

---
+++ Core.Models/OrderItem.cs
@@ -0,0 +1,27 @@
+﻿using System;
+namespace Core.Models
+{
+    public class OrderItem
+    {
+        public Product Product { get; set; }
+        public int Quantity { get; set; }
+        public decimal Subtotal
+        {
+            get
+            {
+                var ret = this.Quantity * this.Product.Price;
+                foreach (var d in this.Product.Discounts)
+                {
+                    if (d is QuantityDiscount qtyDiscount)
+                    {
+                        if (this.Quantity % (qtyDiscount.BuyQty + qtyDiscount.GetFreeQty) == 0)
+                        {
+                            ret = (this.Quantity / (qtyDiscount.BuyQty + qtyDiscount.GetFreeQty) * qtyDiscount.BuyQty) * this.Product.Price;
+                        }
+                    }
+                }
+                return ret;
+            }
+        }
+    }
+}
