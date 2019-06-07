373d08cd3d7aa5cb420cd8557f958463374ccd4b
Kelvin Wong <manchunw@gmail.com>
2019-06-07 19:05:04 +0800

    Change double to decimal

--- Core.Models/Product.cs
+++ Core.Models/Product.cs
@@ -7,8 +7,8 @@ namespace Core.Models
     {
         public string Id { get; set; }
         public string Name { get; set; }
-        public double Quantity { get; set; }
-        public double Price { get; set; }
+        public int Quantity { get; set; }
+        public decimal Price { get; set; }
         public List<Discount> Discounts { get; set; } = new List<Discount>();
     }
 }
