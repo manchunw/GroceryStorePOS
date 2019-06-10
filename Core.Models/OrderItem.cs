ab936c16ec5f82fa4917042da658e41279862cc7
Kelvin Wong <manchunw@gmail.com>
2019-06-10 11:20:41 +0800

    Remove nested if statement

--- Core.Models/OrderItem.cs
+++ Core.Models/OrderItem.cs
@@ -12,12 +12,9 @@ namespace Core.Models
                 var ret = this.Quantity * this.Product.Price;
                 foreach (var d in this.Product.Discounts)
                 {
-                    if (d is QuantityDiscount qtyDiscount)
+                    if (d is QuantityDiscount qtyDiscount && this.Quantity % (qtyDiscount.BuyQty + qtyDiscount.GetFreeQty) == 0)
                     {
-                        if (this.Quantity % (qtyDiscount.BuyQty + qtyDiscount.GetFreeQty) == 0)
-                        {
-                            ret = (this.Quantity / (qtyDiscount.BuyQty + qtyDiscount.GetFreeQty) * qtyDiscount.BuyQty) * this.Product.Price;
-                        }
+                        ret = (this.Quantity / (qtyDiscount.BuyQty + qtyDiscount.GetFreeQty) * qtyDiscount.BuyQty) * this.Product.Price;
                     }
                 }
                 return ret;
